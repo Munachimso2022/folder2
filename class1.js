a556a46a4fcfc6c9bf7bbe86ff29f35ddc71eb94
Munachimso2022 <99395535+Munachimso2022@users.noreply.github.com>
2022-02-11 11:44:43 +0100

    Add files via upload

---
+++ class1.js
@@ -0,0 +1,37 @@
+let person = 'Vincent Ikechukwu';
+
+let peaceMassRoutes = ['Thankgod', 'Muna', 'Joshua', 'Val']
+
+peaceMassRoutes.push('Vincent');
+peaceMassRoutes.pop('Vincent');
+peaceMassRoutes.pop('Muna');
+
+console.log(peaceMassRoutes)
+
+// objects
+
+let carsobject = {
+    maxSpeed:'280km/hr',
+    drive:'four wheel driven',
+    color:'yellow',
+    size:'2000 meters squared'
+};
+
+console.log(carsobject);
+
+let eliteAndTheirCars = {
+    Val:['Land Cruiser', 'Armada'],
+    Vincent:['Tesla Roadster', 'Tesla CyberTruck', 'Lucid Dream'],
+    Muna:['Toyota Hillux', 'Tundra', 'Hummer Pickup'],
+    Thangod:['Range Rover', 'Hillux'],
+    Josh:['Rolls Royce', 'Benz'],
+
+}
+
+eliteAndTheirCars.Vincent = ['Unet Beast', 'Faraday Motors'];
+
+
+console.log(eliteAndTheirCars);
+
+
+
